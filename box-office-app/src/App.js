d230dfea7af0b9ffeabc7d1a502b73b21a7a7c1f
gagan001122 <gaganrajput8437@gmail.com>
2022-07-12 17:26:09 +0530

    added routes

--- box-office-app/src/App.js
+++ box-office-app/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Home from "./Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/starred" />
+
+        <Route path="*" />
+      </Routes>
   );
 }
 
---
+++ box-office-app/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+export default function Home() {
+  return (
+    <div>
+        <p>home</p>
+    </div>
+  )
+}
