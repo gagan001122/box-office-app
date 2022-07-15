804027c6ce2e78682ac184efe82d19bc4a6c6d5e
gagan001122 <gaganrajput8437@gmail.com>
2022-07-15 13:31:46 +0530

    styling entire app

--- box-office-app/src/App.js
+++ box-office-app/src/App.js
@@ -4,6 +4,7 @@ import Navs from './components/Navs';
 import Starred from './pages/Starred';
 import NotFound from './pages/NotFound';
 import Show from './pages/Show';
+import { ThemeProvider } from 'styled-components';
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,8 +12,16 @@ import {
   Link,
   Navigate,
 } from "react-router-dom";
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
 
@@ -22,6 +31,7 @@ function App() {
 
         <Route path="*" element={<NotFound/>} />
       </Routes>
+      </ThemeProvider>
   );
 }
 
